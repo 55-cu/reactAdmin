0183cda30d7a63063fe38d63191d7c91a754b6a1
ty-1423052481 <ty-1423052481>
2020-03-30 16:52:55 +0800

    'tijiao'

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,49 +1,85 @@
 import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import Nav from '../../components/Nav'
 import Modal from '../tokenModal'
 import style from './admin.module.less'
-import { Layout, Breadcrumb,Button } from 'antd';
-import {connect} from 'react-redux'
+import { Layout, Breadcrumb, Button, Dropdown, Menu, Icon } from 'antd';
+import { connect } from 'react-redux'
+import Login from '../../api/login'
 
 const { Header, Content, Footer, Sider } = Layout;
+
 
 class Admin extends Component {
   state = {
     collapsed: false,
-    user: ''
+    user: '',
+    show: false,
+    page:1,
+    pageSize:10,
+    administrator:''
   };
 
   onCollapse = collapsed => {
     this.setState({ collapsed });
   };
-  toLogin=()=>{
+  toLogin = () => {
     this.props.history.push('/login')
+    localStorage.removeItem('token')
   }
   componentDidMount(){
     //获取用户名
     if(localStorage.getItem('user')){
       let result = JSON.parse(localStorage.getItem('user'))
       this.setState({user:result.user})
+
+  // componentDidMount() {
+    
+  //   if (localStorage.getItem('user')) {
+  //     let user = localStorage.getItem('user')
+  //     this.setState({ user })
+  //   }
+    let token = localStorage.getItem('token')
+    if (token) {
+      this.setState({ show: true })
     }
   }
-  exitLogin=()=>{
-    localStorage.setItem('user','')
-    this.setState({user:''})
+  exitLogin = () => {
+    localStorage.setItem('user', '')
+    this.setState({ user: '' })
   }
+
+
   render() {
-    let {tokenModal} = this.props
-    // console.log(tokenModal)
+    const menu = (
+      <Menu>
+        <Menu.Item key='1'>
+          <Button>个人中心</Button>
+        </Menu.Item>
+        <Menu.Item key='2' >
+          <Button onClick={this.toLogin}>退出登录</Button>
+        </Menu.Item>
+      </Menu>)
+    let { tokenModal } = this.props
+    let {show,administrator } = this.state
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        {tokenModal?<Modal></Modal>:''}
+        {tokenModal ? <Modal></Modal> : ''}
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
           <div className={style.logo}>Logo</div>
           <Nav></Nav>
         </Sider>
         <Layout className="site-layout">
           <Header className={style.header} style={{ padding: 0 }}>
-              <Button type="link" onClick={this.toLogin} className={style.login}>登录</Button>
+            <span>小鸡词典</span>
+            <span className={style.bg}></span>
+            {administrator === 1?'超级管理员':'普通管理员'}
+            {!show || <Dropdown overlay={menu}>
+              <a className="ant-dropdown-link" onClick={this.toLogin}>
+                Hover me <Icon type="down" />
+              </a>
+            </Dropdown>}
+            {/* <Button type="link" onClick={this.toLogin} className={style.login}>登录</Button> */}
           </Header>
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
@@ -60,5 +96,5 @@ class Admin extends Component {
     );
   }
 }
-
-export default connect(state=>state)(withRouter(Admin));
+}
+export default connect(state => state)(withRouter(Admin));
